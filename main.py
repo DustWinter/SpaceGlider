8eb06d9d1cdce4f34d41145f2ca5ee36c88a0e40
Corrupted <youssefelajbari@gmail.com>
2024-12-18 13:10:00 +0100

    output

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+
+def Clear():
+    os.system("cls")
+
+def Compress():
+    Clear()
+    os.system("echo Starting Compressing")
+    pass
+
+def Decompress():
+    Clear()
+    os.system("echo Starting Decompressing")
+    pass
